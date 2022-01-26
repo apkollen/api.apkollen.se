2ccbcc057567d7af3e1f0896999ede1f9e1ba777
Emil Jonathan Eriksson <eje1999@gmail.com>
2022-01-26 20:37:02 +0100

    Add maxItems to benchmark lua script

--- benchmark/small_search.lua
+++ benchmark/small_search.lua
@@ -1,6 +1,7 @@
 -- Contains a few product search parameters that will work
--- on either /bs/products/search/(current|all)
+-- on either /bs/products/search/(current|all), as would be likely
+-- for looking at the top 10
 
 wrk.method = "POST"
-wrk.body = '{"productName":["a","e"],"unitVolume":{"max":34},"sortOrder":{"key":"productName","order":"asc"}}'
+wrk.body = '{"productName":["a","e"],"unitVolume":{"max":34},"sortOrder":{"key":"productName","order":"asc"},"maxItems":10}'
 wrk.headers["Content-Type"] = "application/json"
