3e3092459fb71e980e9f1b73b74962672dd2a192
Emil Jonathan Eriksson <eje1999@gmail.com>
2022-01-26 13:04:35 +0100

    Fix adding reviews to reduceDbPostHistoryEntry

--- src/api.ts
+++ src/api.ts
@@ -61,7 +61,8 @@ const addMultipleWhereLikeToQuery = (
 };
 
 /**
- * Select product history coloumn names as camelCase
+ * Select product history coloumn names as camelCase, with optional
+ * review information
  * @param query Query from which to select
  * @param fromTable Table in which `bs_product_article_nbr` is to be taken
  */
@@ -74,7 +75,10 @@ const selectCamelCaseProductHistory = (query: Knex.QueryBuilder, fromTable: stri
     .select('unit_price AS unitPrice')
     .select('alcvol', 'apk')
     .select(`${fromTable}.bs_product_article_nbr AS articleNbr`)
-    .select('retrieved_timestamp AS retrievedTimestamp');
+    .select('retrieved_timestamp AS retrievedTimestamp')
+    .select('score, text')
+    .select('bs_product_review.reviewer_name AS reviewerName')
+    .select('bs_product_review.created_timestamp AS createdTimestamp');
 };
 
 /**
@@ -88,15 +92,29 @@ const reduceDbPosthistoryEntry = (dbpr: DatabaseProductHistoryEntry): ProductHis
     markedAsDead = true;
   }
 
-  const { retrievedTimestamp, ...reduced } = dbpr;
+  const { retrievedTimestamp, score, text, reviewerName, createdTimestamp, ...reduced } = dbpr;
 
-  return {
+  const phe: ProductHistoryEntry = {
     ...reduced,
     markedAsDead,
     retrievedDate: new Date(retrievedTimestamp),
     markedAsDeadDate:
       dbpr.markedAsDeadTimestamp != null ? new Date(dbpr.markedAsDeadTimestamp) : undefined,
   };
+
+  // If review exists, add it
+  if (score != null && text != null && reviewerName != null && createdTimestamp != null) {
+    phe['review'] = {
+      score,
+      text,
+      reviewerName,
+      createdDate: new Date(createdTimestamp)
+    }
+  } else {
+    phe['review'] = null;
+  }
+
+  return phe;
 };
 
 /**
