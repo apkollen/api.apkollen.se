dff8e422e1011614a673af8ba5564322b62388e4
Emil Jonathan Eriksson <eje1999@gmail.com>
2022-01-29 19:20:51 +0100

    Fix db and index eslint

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import app from './app';
 import 'dotenv/config';
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT ?? 3001;
 
 app.listen(PORT ?? 3006, () => {
   console.log(`Listening on http://localhost:${PORT}`);
