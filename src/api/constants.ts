4899885a3ab401bc0a23ae82669687d85977ec34
Emil Jonathan Eriksson <eje1999@gmail.com>
2022-02-08 17:03:27 +0100

    Fix tests for new DB

--- src/api/constants.ts
+++ src/api/constants.ts
@@ -1,3 +1,4 @@
+export const PRODUCT_TABLE = 'bs_product';
 export const PRODUCT_HISTORY_TABLE = 'bs_product_history_entry';
 export const DEAD_LINK_TABLE = 'dead_bs_product';
 export const REVIEW_TABLE = 'bs_product_review';
