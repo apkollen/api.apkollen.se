06c9d863888f54f96ffe914d6fa7da0e1259fc77
Emil Jonathan Eriksson <eje1999@gmail.com>
2022-02-11 23:42:20 +0100

    Update count test

--- test/integration/categories.test.ts
+++ test/integration/categories.test.ts
@@ -1,16 +1,17 @@
-import path = require('path');
-import request = require('supertest');
 import app from '../../src/app';
 
+import path = require('path');
+import request = require('supertest');
+
 // For prisma to find testing DB
-process.env.PRISMA_URL = `file:${path.join(__dirname, '../db/test_db.sqlite')}`
+process.env.PRISMA_URL = `file:${path.join(__dirname, '../db/test_db.sqlite')}`;
 
 const BASE_ROUTE = '/bs/categories';
 
 const KNOWN_CATEGORIES = {
   'Öl & Cider': ['Dansk Öl'],
-  'Vin': ['Rött vin', 'Vitt vin'],
-  'Sprit': ['Likör']
+  Vin: ['Rött vin', 'Vitt vin'],
+  Sprit: ['Likör'],
 };
 
 describe('getting categories', () => {
@@ -19,12 +20,12 @@ describe('getting categories', () => {
 
   it('returns correct categories', async () => {
     const res = await r.get(BASE_ROUTE);
-    const body = res.body as Record<string, string[]>
+    const body = res.body as Record<string, string[]>;
 
     expect(res.statusCode).toEqual(200);
     expect(Object.keys(body)).toHaveLength(CATEGORIES.length);
     expect(Object.keys(body)).toEqual(expect.arrayContaining(CATEGORIES));
-    
+
     // To make TS happy
     expect(body['Öl & Cider']).toEqual(expect.arrayContaining(KNOWN_CATEGORIES['Öl & Cider']));
     expect(body.Vin).toEqual(expect.arrayContaining(KNOWN_CATEGORIES.Vin));
